91ad9659c07fbea91ba2a925ca245306e79fa6c7
Cindy <meridacindy98@yahoo.com.ar>
2020-06-22 02:19:41 -0300

    prueba

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
+import { AgregarPage } from '../pages/agregar/agregar.page';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
+  },
+  {
+    path: 'agregar',
+    component: AgregarPage,
   }
 ];
 
